030eb02748929d353ee37c62d256f11dbaca1a11
Dmitry <r3m.2a@mail.ru>
2022-09-22 11:45:10 +0300

    Worker_threads

--- Worker_threads/index.js
+++ Worker_threads/index.js
@@ -1,8 +1,20 @@
 const crypto=require('crypto');
 const start= performance.now();
+const https=require('https');
+
+// process.env.UV_THREADPOOL_SIZE=8;
+
+// for (let i = 0; i < 50; i++) {
+//     crypto.pbkdf2('test', 'salt',10000,64,'sha512',()=>{
+//         console.log(performance.now()-start);
+//     });
+// }
 
 for (let i = 0; i < 50; i++) {
-    crypto.pbkdf2('test', 'salt',10000,64,'sha512',()=>{
-        console.log(performance.now()-start);
-    })
+    https.get('https://yandex.ru', (res)=>{
+         res.on('data', ()=>{});
+         res.on('end', ()=>{
+                console.log(performance.now()-start);
+         });
+    });
 }
